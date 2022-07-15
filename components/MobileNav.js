dbd3ce3f45c8c4945f55409ae47cd3e6d3d8c0a6
Kirill Sofronov <2138870+sokirill@users.noreply.github.com>
2022-07-15 11:35:50 +0800

    fix font

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -55,7 +55,7 @@ const MobileNav = () => {
         <button
           type="button"
           aria-label="toggle modal"
-          className="fixed·h-full·w-full·cursor-auto·focus:outline-none"
+          className="fixed h-full w-full cursor-auto focus:outline-none"
           onClick={onToggleNav}
         ></button>
         <nav className="fixed mt-8 h-full">
--- pages/_document.js
+++ pages/_document.js
@@ -45,7 +45,7 @@ class MyDocument extends Document {
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
           <link
